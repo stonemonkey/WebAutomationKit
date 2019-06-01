538713c630a0e1930baa4a6f0308f5557f86bdf2
stonemonkey <cmorariu77@yahoo.com>
2019-06-01 19:54:00 +0200

    Fixes package to deploy to .NET Core projects too

---
+++ WebAutomationKit/pack.ps1
@@ -0,0 +1,10 @@
+﻿$NuGetExe = Join-Path $PSScriptRoot '.nuget\nuget.exe'
+
+# download NuGet.exe if missing
+if (-not (Test-Path $NuGetExe)) {
+    Write-Host 'Downloading nuget.exe ...'
+	$client = New-Object System.Net.WebClient
+	$client.DownloadFile('https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe', $NuGetExe)
+}
+
+& $NuGetExe pack WebAutomationKit.nuspec
